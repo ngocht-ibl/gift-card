8502e6998f305ee25eed207fa2fccd0fede44b1a
ngocht2 <ngocht@blockchainlabs.asia>
2024-04-05 09:44:07 +0700

    for testing

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,12 @@
 import { JSX } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
-
+ 
 export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
       disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors"
+      class={`group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none bg-blue-600 text-white hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 ${props.class}`}
     />
   );
 }
